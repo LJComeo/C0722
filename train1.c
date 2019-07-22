38b54718dfae14a0ac6d7e897c321edf1ddf37c8
JC555 <572670014@qq.com>
2019-07-22 12:41:01 +0800

    爱因斯坦阶梯问题

---
+++ train1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int i;
+	for (i = 100; i < 1000; i++)
+	{
+		if (i % 2 == 1 && i % 3 == 2 && i % 5 == 4 && i % 6 == 5 && i % 7 == 0)
+		{
+			printf("%d\n", i);
+		}
+	}
+	system("pause");
+	return 0;
+}
